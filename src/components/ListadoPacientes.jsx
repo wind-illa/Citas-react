00d8b7f87abbeae947e590c1e3ecc91a6580f288
wind peña <nirvanavid@gmail.com>
2024-02-13 16:36:26 -0300

    diseno completado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,20 @@
+import Paciente from "./Paciente"
+
 const ListadoPacientes = () => {
   return (
-    <div className="w-1/2">
-      <h2>Listado de Pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen">
+      <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">Administra tus <span className="text-indigo-600 font-bold ">Pacientes y Citas</span></p>
+      <div className="h-screen md:overflow-y-scroll rounded-xl bg-gray-100">
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+const Paciente = () => {
+  return (
+    <div className="bg-white m-5 mt-0 shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: <span className="font-normal normal-case">Hook</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case">Juan</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Email: <span className="font-normal normal-case">Correo@correo.com</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: <span className="font-normal normal-case">10 Diciembre de 2022</span></p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, eum laboriosam omnis ut ducimus ad quis aut possimus voluptatibus voluptas sequi natus est libero et velit odio non harum minus.</span></p>
+    </div>
+  )
+}
+
+export default Paciente
